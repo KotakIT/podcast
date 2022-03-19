0218ff81ee19e0984ba3fa69211efd4f19934b92
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-03-19 11:54:17 +0700

    perbarui tampilan

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,28 @@
 <template>
-  <footer>
-    <div>
-      by @raflidev
-    </div>
-    <div>
-      <a class="text-white" href="http://instagram.com/raflidev">
-        <u>
-          ig
-        </u>
-      </a>
-      /
-      <a class="text-white" href="http://twitter.com/raflidev">
-        <u>tw</u>
-      </a>
-      /
-      <a class="text-white" href="http://github.com/raflidev">
-        <u>ghub</u>
-      </a>
+  <footer class="">
+    <div class="flex justify-center text-center">
+      <div class="w-full md:w-1/2 lg:w-1/3 bg-gray-900">
+        <div class="flex justify-evenly">
+          <div>
+            by @raflidev
+          </div>
+          <div>
+            <a class="text-white" href="http://instagram.com/raflidev">
+              <u>
+                ig
+              </u>
+            </a>
+            /
+            <a class="text-white" href="http://twitter.com/raflidev">
+              <u>twt</u>
+            </a>
+            /
+            <a class="text-white" href="http://github.com/raflidev">
+              <u>github</u>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
@@ -25,19 +31,3 @@
 export default {};
 </script>
 
-<style>
-footer {
-  display: flex !important;
-  justify-content: space-between;
-  align-items: center;
-  position: fixed;
-  bottom: 0px;
-  left: 0px;
-  transform: translate(-50%);
-  margin-left: 50%;
-  padding: 8px 5%;
-  background: #2a343e;
-  width: 100%;
-  max-width: 428px;
-}
-</style>
